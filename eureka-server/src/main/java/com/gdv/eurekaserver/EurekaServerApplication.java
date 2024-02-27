7d29f562b403733ded90a84b08d63488db473414
Gopal Dutt Vishwakarma <gopalduttvishwakarma@gmail.com>
2024-02-27 16:46:11 +0530

    Added author name

--- eureka-server/src/main/java/com/gdv/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/gdv/eurekaserver/EurekaServerApplication.java
@@ -3,13 +3,13 @@ package com.gdv.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/*
+* Author - Gopal Dutt Vishwakarma
+* */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
-
 }
